1fccf3c375f3bf92ae8d414bf3e0b58965bea3ec
dnr00 <36317567+dnr00@users.noreply.github.com>
2023-02-23 19:15:25 +0900

    Add files via upload

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -0,0 +1,90 @@
+package org.zerock.jdbcex.dao;
+
+import com.sun.tools.javac.comp.Todo;
+import lombok.Cleanup;
+import org.checkerframework.checker.units.qual.C;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoDAO {
+
+    public String getTime() {
+
+        String now = null;
+
+        try(Connection connection = ConnectionUtil.INSTANCE.getConnection();
+            PreparedStatement preparedStatement = connection.
+                                prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ) {
+
+            resultSet.next();
+
+            now = resultSet.getString(1);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return now;
+    }
+
+    public String getTime2() throws Exception {
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.
+                                                    prepareStatement("select now()");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
+
+        return now;
+    }
+
+    public void insert(TodoVO vo) throws Exception {
+        String sql = "insert into tbl_todo (title, dueDate, finished) value (?, ?, ?)";
+
+
+        @Cleanup Connection connecton = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connecton.prepareStatement(sql);
+
+
+        preparedStatement.setString(1, vo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(vo.getDueDate()));
+        preparedStatement.setBoolean(3, vo.isFinished());
+
+        preparedStatement.executeUpdate();
+    }
+
+    public List<TodoVO> selectAll()throws Exception {
+
+        String sql = "select * from tbl_todo";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+
+        List<TodoVO> list = new ArrayList<>();
+
+        while(resultSet.next()) {
+            TodoVO vo = TodoVO.builder()
+                    .tno(resultSet.getLong("tno"))
+                    .title(resultSet.getString("title"))
+                    .dueDate( resultSet.getDate("dueDate").toLocalDate())
+                    .finished(resultSet.getBoolean("finished"))
+                    .build();
+
+            list.add(vo);
+        }
+
+
+        return list;
+    }
+}
